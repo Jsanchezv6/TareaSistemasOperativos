d4469a30dc46832e716fc4a2a0c30618561b9fa5
Jsanchezv6 <85458422+Jsanchezv6@users.noreply.github.com>
2023-08-02 21:46:20 -0600

    commit
    tercera version

--- Holamundo/src/holamundo.java
+++ Holamundo/src/holamundo.java
@@ -1,6 +1,6 @@
 
 public class holamundo {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo mi nombre es Jenner!");
+        System.out.println("¡Hola Mundo mi nombre es Jenner y tengo 20 años!");
     }
 }
